5152eab41bbfc541e4e1cfb11b4b8c4f03198d72
Max <zeetreex@gmail.com>
2023-02-12 21:45:34 +0200

    init

---
+++ src/main/java/com/jwtexample/security/JwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.jwtexample.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthApplication.class, args);
+	}
+
+}
