66ddc06575266f7eb2b7ac9d479d36b8e42e97f2
Max <zeetreex@gmail.com>
2023-02-15 18:56:55 +0200

    getSignKey, extractAllClaims and extractClaim methods

--- src/main/java/com/jwtexample/security/config/JwtService.java
+++ src/main/java/com/jwtexample/security/config/JwtService.java
@@ -1,11 +1,40 @@
 package com.jwtexample.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 
 public class JwtService {
+
+	private static final String SECRET_KEY = "703373367639792442264528482B4D6251655468576D5A7134743777217A2543";
+
 	public String extractUsername(String token) {
 		return null;
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts
+						.parserBuilder()
+						.setSigningKey(getSignKey())
+						.build()
+						.parseClaimsJws(token)
+						.getBody();
+	}
+
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+		return Keys.hmacShaKeyFor(keyBytes);
 	}
 }
